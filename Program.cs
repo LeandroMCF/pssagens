60b9e3d6632debc45d447fdbc3d7476cba836420
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-11-26 19:26:05 -0300

    Finalizando Passagens

--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ namespace pssagens
             int rep;
             string[] nomes = new string[5], partida = new string[5], chegada = new string[5]; 
             double[] data = new double[5];
+            int contador = 0;
 
             rep = 0;
-            Console.Write("------------------\nPassagens\n------------------\nDigite sua senha:");
+            Console.Write("------------------\nPassagens\n------------------\nDigite sua senha: ");
             senha = Console.ReadLine();
             login(senha);
 
@@ -23,7 +24,6 @@ namespace pssagens
                 switch (rep)
                 {
                     case 1:
-                        int contador = 0;
                         string resp;
                         do
                         {
@@ -35,7 +35,7 @@ namespace pssagens
                                 partida[contador] = Console.ReadLine();
                                 Console.WriteLine($"Digite o ponto de chegada do {contador + 1}º passageio");
                                 chegada[contador] = Console.ReadLine();
-                                Console.WriteLine($"Digite a data da viagem do {contador + 1}º passageio");
+                                Console.WriteLine($"Digite a data da viagem do {contador + 1}º passageio (DD.MM.AAA)");
                                 data[contador] = double.Parse(Console.ReadLine());
                                 contador++;
                             }
@@ -46,25 +46,39 @@ namespace pssagens
                             Console.WriteLine($"Deseja adicionar mais algum passageiro?\n[S] Para sim\n[N] Para não");
                             resp = Console.ReadLine();
                             resp = resp.ToUpper();
+                            while (resp != "S" && resp != "N")
+                            {
+                                Console.WriteLine($"Opição inválida.\n[S] Para sim\n[N] Para não");
+                                resp = Console.ReadLine();
+                                resp = resp.ToUpper();
+                            }
                         } while (resp == "S");
                     break;
 
                     case 2:
-                        
+                        while (contador == 0)
+                        {
+                            Console.WriteLine($"Você ainda não adicionou nada nenhum passageiro.");
+                            Console.WriteLine("----------------------");
+                            break;
+                        }
+                        for (var i = 0; i < contador; i++)
+                        {
+                            Console.WriteLine("----------------------");
+                            Console.WriteLine(nomes[i]);
+                            Console.WriteLine($"Vai partir de {partida[i]}");
+                            Console.WriteLine($"Vai chegar em {chegada[i]}");
+                            Console.WriteLine($"No dia {data[i]}");
+                            Console.WriteLine("----------------------");
+                        }
+                    break;
+                    
+                    default:
+                        Console.WriteLine("Ok, obrigado. Tchau!");
                     break;
                 }
                 
             } while (rep != 0);
-
-
-
-
-
-
-
-
-
-
 
 
             //Funções
@@ -79,7 +93,7 @@ namespace pssagens
                     }
                     else
                     {
-                        Console.WriteLine($"Senha incorreta");
+                        Console.Write($"Senha incorreta.\nDigite a senha novamente: ");
                         senha = Console.ReadLine();
                     }
                 } while (senhaInvalida != true);
